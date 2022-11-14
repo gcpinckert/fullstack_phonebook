87a62b747f46674406859849104b3b8f90ae51b5
Ginni Pinckert <gcpinckert@gmail.com>
2022-11-14 14:39:49 -0800

    Basic CRUD operations (read, add, delete)

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   { 
     "id": 1,
@@ -31,6 +33,58 @@ app.get('/api/persons', (request, response) => {
 app.get('/info', (request, response) => {
   const template = `<p>Phonebook has info for ${persons.length} people</p><p>${String(new Date())}</p>`;
   response.send(template);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+
+  response.status(204).end();
+});
+
+const generateUniqueId = () => {
+  let ids = persons.map(person => person.id);
+  let newId = ids[0];
+
+  while(ids.includes(newId)) {
+    newId = Math.floor(Math.random() * (500 - 1) + 1);
+  }
+
+  return newId;
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'must provide values for name and number',
+    });
+  } else if (persons.map(p => p.name).includes(body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique',
+    });
+  }
+
+  const person = {
+    id: generateUniqueId(),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(person);
+  response.json(person);
 });
 
 const PORT = 3001;
