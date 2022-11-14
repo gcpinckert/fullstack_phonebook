14ac88d58916a4b2ece9dc4e60b923a863f08251
Ginni Pinckert <gcpinckert@gmail.com>
2022-11-14 14:14:31 -0800

    Server provides general information

--- index.js
+++ index.js
@@ -28,6 +28,11 @@ app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
 
+app.get('/info', (request, response) => {
+  const template = `<p>Phonebook has info for ${persons.length} people</p><p>${String(new Date())}</p>`;
+  response.send(template);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
