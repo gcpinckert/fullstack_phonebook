2a1774fdf7c1b7efec4c9fb3e67993991d1add33
Ginni Pinckert <gcpinckert@gmail.com>
2022-11-16 10:23:54 -0800

    Deployment

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person);
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
